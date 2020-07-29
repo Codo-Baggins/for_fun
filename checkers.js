80c426f4dad8d4c31281f4ac9cb7456d8c903f29
Joe Lopez <lopez.4.joseph@gmail.com>
2020-07-29 12:18:59 -0600

    Updated algorithm

--- checkers.js
+++ checkers.js
@@ -1,37 +1,61 @@
 
 
-var xAxis = []
+var xAxis = [];
+
+var yAxis = [];
+
+wholeBoard = [];
 
 function increaseX(size) {
         if (size % 2 == 0) {
-            while (xAxis.length < size) {
-                xAxis.push(" ", "X");
+            for (var i = 0; i < size; i++) {
+                i = xAxis.push(" ", "X");
             } 
+            for (var j = 0; j< size; j++) {
+                j = yAxis.push("X", " ");
+            }
         } else if (size % 2 == 1) {
-            while (xAxis.length < size) {
-                xAxis.push(" ");
+            for (var i = 0; i < size; i++) {
+                i = xAxis.push(" ");
                 if (xAxis.length == size) {
                     break;
                 }
                 xAxis.push("X");
             }
+            for (var j = 0; j < size; j++) {
+                    j = yAxis.push("X");
+                    if (yAxis.length == size) {
+                        break;
+                    }
+                    yAxis.push(" ");
+                }
         } else {
             console.log("what?!!");
         }
 }
 
-console.log(xAxis);
-
-var numberSpaces = 3;
+var numberSpaces = 6;
 
 increaseX(numberSpaces);
 
-var everyOther = xAxis.pop(xAxis.unshift("X"));
-
-for (i = 0; i < numberSpaces; i++) {
+function shiftOverOne() {
+    xAxis.pop();
+    xAxis.unshift("X");
     console.log(xAxis);
-    console.log(everyOther);
-    
 }
 
- 
+
+function partOne() {
+    for (i = 0; i < numberSpaces; i++) {
+        i = wholeBoard.push(xAxis);
+        if (wholeBoard.length == numberSpaces) {
+            break;
+        }
+        wholeBoard.push(yAxis);
+    }    
+}
+
+partOne();
+
+console.log(wholeBoard);
+
